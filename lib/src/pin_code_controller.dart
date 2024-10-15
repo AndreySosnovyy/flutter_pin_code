ae9f03d76ffdb1b500ebf47ba113e0e3af969415
Andrey Sosnovyy <andrey.sosnovyy@umbrellait.com>
2024-10-15 11:36:55 +0300

    Remove todo

--- lib/src/pin_code_controller.dart
+++ lib/src/pin_code_controller.dart
@@ -29,7 +29,6 @@ const String _kBiometricsTypeKeySuffix = '.biometrics';
 const String _kBackgroundTimestampKey = 'flutter_pin_code.background_timestamp';
 
 // TODO(Sosnovyy): write and read timeouts every time from disk
-// TODO(Sosnovyy): add getter for available biometrics type
 /// {@template flutter_pin_code.pin_code_controller}
 /// Controller for working with pin code related features.
 /// {@endtemplate}
